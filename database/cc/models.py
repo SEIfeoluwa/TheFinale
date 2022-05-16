8e21e3f14c39e9d7c5936a9b81bb025d7de396e8
Ifeoluwa Adebisi <adebisiife@gmail.com>
2022-05-16 00:34:41 -0400

    prep for custom user model

--- database/cc/models.py
+++ database/cc/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 
 # Create your models here.
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     username = models.CharField(max_length=100, unique=True)
