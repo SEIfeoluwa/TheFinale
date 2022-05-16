d0ff9fd0b95848905b7e34cca4a27b5b452a3aef
Ifeoluwa Adebisi <adebisiife@gmail.com>
2022-05-16 11:04:21 -0400

    changes

--- database2/ctrlc/forms.py
+++ database2/ctrlc/forms.py
@@ -6,3 +6,9 @@ class DeviceForm(forms.ModelForm):
     class Meta:
         model = Device
         fields = ('user', 'company', 'name', 'label', 'features', 'description', 'company', 'location',)
+
+class UserForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'user_name', 'email', 'address', 'photo_url')
