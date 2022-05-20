5295fd1e91bd900727ef5c23757d5d3c301fb0f3
Ifeoluwa Adebisi <adebisiife@gmail.com>
2022-05-20 12:36:07 -0400

    user list view

--- database2/ctrlc/urls.py
+++ database2/ctrlc/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
     path('device/', views.device_list, name='device_list'),
     path('company/', views.company_list, name='company_list'),
     path('users/<int:pk>', views.user_detail, name='user_detail'),
-    path('', views.device_detail, name='device_detail'),
-    path('', views.company_detail, name='company_detail'),
-    path('', views.device_create, name='device_create'),
-    path('', views.device_edit, name='device_edit'),
-    path('', views.user_delete, name='user_delete'),
-    path('', views.device_delete, name='device_delete')
+    path('device/<int:pk>', views.device_detail, name='device_detail'),
+    path('company/<int:pk>', views.company_detail, name='company_detail'),
+    path('device/add', views.device_create, name='device_create'),
+    path('device/<int:pk>/edit', views.device_edit, name='device_edit'),
+    path('user/<int:pk>/delete', views.user_delete, name='user_delete'),
+    path('device/<int:pk>/delete', views.device_delete, name='device_delete')
 ]
