c613f52a4a3e6b46d52ea3b24015d500ac0825ca
Ifeoluwa Adebisi <adebisiife@gmail.com>
2022-05-20 16:17:30 -0400

    login

--- thefinale client/src/pages/Login.js
+++ thefinale client/src/pages/Login.js
@@ -3,9 +3,34 @@ import { Link } from 'react-router-dom'
 
 const Login = (props) => {
     
+    const handleSubmit = () => {
+        Navigate('/home')
+    }
+
     return (
         <div>
-            
+            <div className="login"> 
+            <h1 className="reg-title">Ctrlc</h1>
+                <form className='log' onSubmit={handleSubmit}>
+                    <div className="welcome"> Please Log in</div>
+                    <div className='input-wrapper'>
+                        <label htmlFor='email'>Username</label>
+                         <input className="input2"
+                            name="username"
+                            type="text"
+                            placeholder="Your Username"
+                            value='Elonccentric'
+                        />
+                        <label htmlFor="password">Password</label>
+                        <input className="input2"
+                        type="password"
+                        name="password"
+                        value='KevinSucks'
+                        />
+                    </div>
+                    <button className="s-btn1" >Log In</button>
+                </form> 
+            </div>
         </div>
     )
 }
