e71ebdb59d9f1e249ea2203a0b49901a942b4799
Ifeoluwa Adebisi <adebisiife@gmail.com>
2022-05-20 16:55:11 -0400

    partners

--- thefinale client/src/pages/Learn.js
+++ thefinale client/src/pages/Learn.js
@@ -1,10 +1,21 @@
 
 
 const Learn = () => {
+    const [partners, setPartners] = useState([])
+
+    const getData = async () => {
+        const res = await Client.get('company/')
+        setPartners(res.data)
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
 
     return (
         <div className="Learnmore">
-
+            {partners}
         </div>
     )
 }
