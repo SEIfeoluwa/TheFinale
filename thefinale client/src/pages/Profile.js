bdd1d8ba4781e5bb4bf75b464d6130bacbd1af39
Ifeoluwa Adebisi <adebisiife@gmail.com>
2022-05-20 16:53:23 -0400

    edit

--- thefinale client/src/pages/Profile.js
+++ thefinale client/src/pages/Profile.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import Client from '../services/api'
 
 const Profile = () => {
-    const [data, setData] = useState([])
+    const [data, setData] = useState()
 
     const getData = async () => {
         const res = await Client.get('users/2')
