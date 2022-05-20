082229d0402ab5103ccea6f43a2a112125ff8267
Ifeoluwa Adebisi <adebisiife@gmail.com>
2022-05-20 11:17:31 -0400

    halfdone Urls

--- database2/ctrlc/urls.py
+++ database2/ctrlc/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.user_list, name='user_list'),
+    path('device/', views.device_list, name='device_list'),
+    path('company/', views.company_list, name='company_list'),
+    path('users/<int:pk>', views.user_detail, name='user_detail'),
+    path('', views.device_detail, name='device_detail'),
+    path('', views.company_detail, name=''),
+    path('', views.device_create, )
 ]
