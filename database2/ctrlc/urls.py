41a43abf790f6677eb85ce49138feaff18b92315
Ifeoluwa Adebisi <adebisiife@gmail.com>
2022-05-20 11:39:30 -0400

    url name

--- database2/ctrlc/urls.py
+++ database2/ctrlc/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('company/', views.company_list, name='company_list'),
     path('users/<int:pk>', views.user_detail, name='user_detail'),
     path('', views.device_detail, name='device_detail'),
-    path('', views.company_detail, name=''),
-    path('', views.device_create, )
+    path('', views.company_detail, name='company_detail'),
+    path('', views.device_create, name='device_create'),
+    path('', views.device_edit, name='device_edit'),
+    path('', views.user_delete, name='user_delete'),
+    path('', views.device_delete, name='device_delete')
 ]
